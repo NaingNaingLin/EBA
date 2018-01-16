b921592a0f8bd851de3374c846970c0b948d738a
Naing Naing Lin <Naing Naing Lin@DESKTOP-N21KS07>
2018-01-16 18:25:36 +0630

    commit 1

---
+++ src/main/java/com/nnl/config/EbaProjectApplication.java
@@ -0,0 +1,12 @@
+package com.nnl.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbaProjectApplication.class, args);
+	}
+}
